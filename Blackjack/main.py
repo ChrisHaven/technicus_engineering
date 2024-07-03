50a36bddd5a2979b3e0e0b95d7ed92cc3e7f4256
Chris van der Haven <chris.van.der.haven@gmail.com>
2024-07-03 16:28:57 +0200

    started working on splitting

--- Blackjack/main.py
+++ Blackjack/main.py
@@ -11,17 +11,18 @@ from dealer import calculate_dealer_value
 
 import time
 
-card_images = [] #prevents garbage collection
+card_images = [] # prevents garbage collection
+player_cards_icons = [] # to be able to remove cards when splitting
 
-#commands
+# commands
 def hit_command(hand):
     global player_hand
     global player_value
 
-    player_hand = hit(deck, hand)
+    player_hand = player.hit(deck, hand)
 
     #calculates player value and prints it
-    player_value = calculate_player_value(player_hand)
+    player_value = player.calculate_player_value(player_hand)
     if player_value[1] > 0:
         player_display_value = f'{player_value[0]}/{player_value[0]-10}'
     else:
@@ -36,18 +37,39 @@ def hit_command(hand):
     # print(f'cards/{player_hand[len(player_hand)-1][0]}_of_{player_hand[len(player_hand)-1][1]}')
     # print(len(player_hand))
 
-def add_card(frame, card_path): #function to display card
+def split_command(hand):
+    hands = player.split(deck, hand)
+
+    hand1 = hands[0]
+    hand2 = hands[1]
+
+    if hands[0] == 'error':
+        messagebox.showinfo(title='error', message='je mag alleen splitten als beide waardes gelijk zijn en als er nog niet is gehit of gesplit!')
+    else:
+        print(f'hand1: {hand1}, hand2: {hand2}')
+
+        for card in player_cards_icons:
+            card.destroy()
+        player_cards_icons.clear()
+        
+        player_frame_double.pack(fill="both", expand="yes", before=button_frame)
+        add_card(player_frame, f'cards/{hand1[0][0]}_of_{hand1[0][1]}.png')
+        add_card(player_frame, f'cards/{hand1[1][0]}_of_{hand1[1][1]}.png')
+        add_card(player_frame_double, f'cards/{hand2[0][0]}_of_{hand2[0][1]}.png')
+        add_card(player_frame_double, f'cards/{hand2[1][0]}_of_{hand2[1][1]}.png')
+
+def add_card(frame, card_path): # function to display card
     original_img = Image.open(card_path)
-    scaled_img = original_img.resize((125,182)) #original: 500x726
+    scaled_img = original_img.resize((100,145)) # original: 500x726
     img = ImageTk.PhotoImage(scaled_img)
-    card_images.append(img) #prevents garbage collection
+    card_images.append(img) # prevents garbage collection
     card = tk.Label(frame, image=img)
     card.pack(side="left", padx=5, pady=5)
     return card
 
 def add_hidden_card(frame):  # function to display hidden card
     original_img = Image.open("cards/back.png")
-    scaled_img = original_img.resize((125, 182))
+    scaled_img = original_img.resize((100,145))
     img = ImageTk.PhotoImage(scaled_img)
     card_images.append(img)
     card = tk.Label(frame, image=img)
@@ -56,7 +78,7 @@ def add_hidden_card(frame):  # function to display hidden card
 
 def reveal_card(card_label, card_path):  # function to reveal hidden card
     original_img = Image.open(card_path)
-    scaled_img = original_img.resize((125, 182))
+    scaled_img = original_img.resize((100,145))
     img = ImageTk.PhotoImage(scaled_img)
     card_images.append(img)
     card_label.configure(image=img)
@@ -68,9 +90,9 @@ def add_chip_button():
         add = float(add)
         player.add_chips(add)
         chip_display.config(text=f"chips: {player.chips}")
-        #print(f'digit {add}')
+        # print(f'digit {add}')
     else:
-        #print('not digit')
+        # print('not digit')
         messagebox.showinfo(title="ongeldig getal", message="Vul een geldig getal in! Geen comma getallen of letters!")
 
 #start of GUI
@@ -79,35 +101,39 @@ root.title('Blackjack')
 root.iconbitmap('icon.ico')
 root.state('zoomed')
 
-#dealer frame
+# dealer frame
 dealer_frame = tk.Frame(root) # frame that the cards will be in
 dealer_frame.pack(fill="both", expand="yes")
 dealerstr = tk.Label(dealer_frame, text="dealer", height=1)
 dealerstr.pack()
 
-#player frame
+# player frame
 player_frame = tk.Frame(root) # frame that the cards will be in
 player_frame.pack(fill="both", expand="yes")
 playerstr = tk.Label(player_frame, text="speler", height=1)
 playerstr.pack()
 
-#chip entry
+# player frame double
+player_frame_double = tk.Frame(root) # frame that the cards will be in
+# player_frame_double.pack(fill="both", expand="yes")
+
+# chip entry
 chip_entry = tk.Entry(player_frame)
 chip_entry.pack()
 chip_entry_botton = tk.Button(player_frame, text="deposit chips", font= ("Arial", 11), height=1, command=add_chip_button)
 chip_entry_botton.pack()
 
-#chip amount
+# chip amount
 chip_display = tk.Label(player_frame, text=f"chips: {player.chips}", font=("Arial", 14))
 chip_display.pack()
 
-#button frame
+# button frame
 button_frame = tk.Frame(root)
 for i in range(4):
     button_frame.columnconfigure(i, weight=1)
 button_frame.pack(fill="x")
 
-#buttons
+# buttons
 button_height = 3
 
 doubleButton = tk.Button(button_frame, text='Double', font=('Arial', 18), height=button_height, bg="orange", fg="white", command=double)
@@ -119,18 +145,21 @@ hitButton.grid(row=0, column=1, sticky=tk.W+tk.E)
 standButton = tk.Button(button_frame, text='Stand', font=('Arial', 18), height=button_height, bg="firebrick", fg="white", command=stand)
 standButton.grid(row=0, column=2, sticky=tk.W+tk.E)
 
-splitButton = tk.Button(button_frame, text='Split', font=('Arial', 18), height=button_height, bg="dodger blue", fg="white", command=split)
+splitButton = tk.Button(button_frame, text='Split', font=('Arial', 18), height=button_height, bg="dodger blue", fg="white", command=lambda: split_command(player_hand))
 splitButton.grid(row=0, column=3, sticky=tk.W+tk.E)
 
-#creating deck and dealing cards
+# creating deck and dealing cards
 deck = new_deck()
 dealer_hand = deal_cards(deck)
-player_hand = deal_cards(deck)
+# player_hand = deal_cards(deck)
+val1 = 10
+val2 = val1
+player_hand = [(val1, 'spades'), (val2, 'spades')]
 player.hand = player_hand
 
 
 
-#calculates dealers value and hows it (not used first round to prevent giving hidden card)
+# calculates dealers value and hows it (not used first round to prevent giving hidden card)
 dealer_value = calculate_dealer_value(dealer_hand)
 if dealer_value[1] > 0:
     dealer_display_value = f'{dealer_value[0]}/{dealer_value[0]-10}'
@@ -141,14 +170,11 @@ dealer_val = tk.Label(dealer_frame, text=dealer_display_value, height=1, font=('
 dealer_val.pack()
 
 hidden_card = add_hidden_card(dealer_frame)
-#print(dealer_hand) #used for testing
+# print(dealer_hand) #used for testing
 add_card(dealer_frame, f"cards/{dealer_hand[0][0]}_of_{dealer_hand[0][1]}.png")
-#add_hidden_card() #adds hidden card
+# add_hidden_card() #adds hidden card
 
-
-
-
-#calculates player value and prints it
+# calculates player value and prints it
 player_value = calculate_player_value(player_hand)
 if player_value[1] > 0:
     player_display_value = f'{player_value[0]}/{player_value[0]-10}'
@@ -158,9 +184,9 @@ else:
 player_val = tk.Label(player_frame, text=player_display_value, height=1, font=('arial', 14))
 player_val.pack()
 
-#print(player_hand) #used for testing
-add_card(player_frame, f"cards/{player_hand[0][0]}_of_{player_hand[0][1]}.png")
-add_card(player_frame, f"cards/{player_hand[1][0]}_of_{player_hand[1][1]}.png")
+# print(player_hand) #used for testing
+player_cards_icons.append(add_card(player_frame, f"cards/{player_hand[0][0]}_of_{player_hand[0][1]}.png"))
+player_cards_icons.append(add_card(player_frame, f"cards/{player_hand[1][0]}_of_{player_hand[1][1]}.png"))
 
 
 
--- Blackjack/player.py
+++ Blackjack/player.py
@@ -1,7 +1,12 @@
 from cards import *
 
 hand = []
+
 chips = float(25)
+
+hit_status = False # to make sure you cant split or double after hitting
+split_status = False # to make sure you cant split twice
+next_hand = False # to be able to go to the second hand after splitting
 
 def double():
     print('double')
@@ -9,13 +14,49 @@ def double():
 def hit(deck, hand):
     new_hand = add_new_card(hand, deck)
     print('hit')
-    return(new_hand)
+    global hit_status #to prevent doubeling/splitting after hitting
+    hit_status = True
+    return new_hand
 
 def stand():
+    if split_status == True:
+        next_hand == True
+    hit_status = False
     print('stand')
 
-def split():
-    print('split')
+def split(deck, hand):
+    global split_status
+    print(f'split: {split_status}, hit: {hit_status}') 
+    val1 = hand[0][0]
+    val2 = hand[1][0]
+    
+    hand1 = []
+    hand2 = []
+
+    if val1 == 'jack' or 'queen' or 'king':
+        val1 == 10
+    elif val1 == 'ace':
+        val1 == 11
+
+    if val2 == 'jack' or 'queen' or 'king':
+        val2 == 10
+    elif val2 == 'ace':
+        val2 == 11
+
+    if val1 == val2 and hit_status == False and split_status == False:
+        split_status = True
+        
+        hand1.append(hand[0])
+        hand1 = add_new_card(hand1, deck)
+        # print(hand1)
+
+        hand2.append(hand[1])
+        hand2 = add_new_card(hand2, deck)
+        # print(hand2)
+    else:
+        hand1 = 'error'
+        hand2 = 'error'
+    return hand1, hand2
 
 def calculate_player_value(player_hand):
     player_value = calculate_value(player_hand)
